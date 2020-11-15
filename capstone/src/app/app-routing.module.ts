ccfce5622084a61f21e33266ffde7fa8bcd301ee
wana155 <apere129@ucr.edu>
2020-11-15 12:26:54 -0600

    Added routing between application components

--- capstone/src/app/app-routing.module.ts
+++ capstone/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CartComponent } from './cart/cart.component';
+import { InComponent } from './in/in.component';
+import { ShopComponent } from './shop/shop.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'shop', component: ShopComponent},
+  {path: 'in', component: InComponent},
+  {path: 'cart', component: CartComponent},
+  {path: '', redirectTo: 'shop', pathMatch: 'full' }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
