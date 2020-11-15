cfc2f494ace72ee2214319774312c76e662d94f7
wana155 <apere129@ucr.edu>
2020-11-15 12:46:56 -0600

    Changed the navigation bar stylings and

---
+++ capstone/src/app/product.model.ts
@@ -0,0 +1,9 @@
+export class Product{
+    constructor(
+        public id: string,
+        public title:string,
+        public price:number,
+        public size:string,
+        public description:string,
+        public images:number){}
+}
