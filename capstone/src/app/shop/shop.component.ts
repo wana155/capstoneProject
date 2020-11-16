ae3b0b548b7d211ae921b01bd13cccfe964e4806
wana155 <apere129@ucr.edu>
2020-11-16 10:08:55 -0600

    Added class variables to store databse products. Wroking and fisplaying with mock products

--- capstone/src/app/shop/shop.component.ts
+++ capstone/src/app/shop/shop.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
 
 @Component({
   selector: 'app-shop',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shop.component.css']
 })
 export class ShopComponent implements OnInit {
+  products:Array <Product>;
 
-  constructor() { }
+  constructor() { 
+    this.products = new Array<Product>();
+    //Temporary:
+   var  product = new Product("100","Flex Pants",25,"Large"," - 56% Cotton, 44% Polyester -Imported - Drawstring closure - Machine Wash - A relaxed leg and elastic, drawstring waistband bring lounge-ready style to this classic casual pant - Elastic cuffs at ankle and on-seam side pockets -Everyday made better: we listen to customer feedback and fine-tune every detail to ensure quality, fit, and comfort ",55);
+   this.products.push(product); 
+   var  product = new Product("200","Shirt",12,"Small"," - 56% Cotton, 44% Polyester -Imported - Drawstring closure - Machine Wash - A relaxed leg and elastic, drawstring waistband bring lounge-ready style to this classic casual pant - Elastic cuffs at ankle and on-seam side pockets -Everyday made better: we listen to customer feedback and fine-tune every detail to ensure quality, fit, and comfort ",55);
+   this.products.push(product); 
+   
+   //T
+  }
 
   ngOnInit(): void {
   }
